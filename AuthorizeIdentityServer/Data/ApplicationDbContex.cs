837a69aad5015be8fd1daa9f960bda1e6eef79fb
Artemij <akolesov408@gmail.com>
2022-05-11 21:23:35 +0300

    a

--- AuthorizeIdentityServer/Data/ApplicationDbContex.cs
+++ AuthorizeIdentityServer/Data/ApplicationDbContex.cs
@@ -1,7 +1,11 @@
-﻿namespace AuthorizeIdentityServer.Data
-{
-    public class ApplicationDbContex
-    {
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
+namespace AuthorizeIdentityServer.Data
+{
+    public class ApplicationDbContex: IdentityDbContext<>
+    {
+        public ApplicationDbContex(D)
+        {
+        }
     }
 }
