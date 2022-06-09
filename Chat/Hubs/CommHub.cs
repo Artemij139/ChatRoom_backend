dd8e548b6bfd5c0f7e192b6eab298d76e7f94958
Artemij <akolesov408@gmail.com>
2022-06-09 22:50:11 +0300

    refactoring

--- Chat/Hubs/CommHub.cs
+++ Chat/Hubs/CommHub.cs
@@ -30,44 +30,43 @@ namespace Chat.Hubs
 
             _manager.ConnectUser(userName, connectionId);
 
-            await UpdateUsersAsync();
+            await UpdateUsersAsync(); 
 
-            await UpdateMessagesAsync();
+            await UpdateMessagesAsync(); 
 
-            await base.OnConnectedAsync();
-
-
-
+            await base.OnConnectedAsync();      
         }
-        public override async Task OnDisconnectedAsync(Exception? exception)
+
+        //When client client, he get all history messages
+        public Task UpdateMessagesAsync()
         {
-            var isUserRemoved = _manager.DisconnectUser(Context.ConnectionId);
-            await UpdateUsersAsync();
-            await base.OnDisconnectedAsync(exception);
+            var mess = _chatDb.messages.ToList();
+            Clients.Caller.UpdateMessagesAsync(mess);
+            return Task.CompletedTask;
         }
 
         public Task UpdateUsersAsync()
         {
-            var users = _manager.Users.Select(x=>x.Name).ToList();
+            var users = _manager.Users.Select(x => x.Name).ToList();
             Clients.All.UpdateUsersAsync(users);
             return Task.CompletedTask;
         }
 
-        public Task UpdateMessagesAsync()
+        public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            var mess = _chatDb.messages.ToList();
-            Clients.All.UpdateMessagesAsync(mess);
-            return Task.CompletedTask;
+            var isUserRemoved = _manager.DisconnectUser(Context.ConnectionId);
+            if (isUserRemoved) { await UpdateUsersAsync(); }
+            await base.OnDisconnectedAsync(exception);
         }
 
-
+        
         [Authorize(Policy = "HasName")]
         public async Task SendMessageAsync(string userName, string message)
         {
             
             //change All to Others
             await Clients.All.SendMessageAsync(userName, message);
-            var x = await _chatDb.messages.AddAsync(
+            await _chatDb.messages.AddAsync(
                 new Message
                 {   
                     Id = Guid.NewGuid(),
