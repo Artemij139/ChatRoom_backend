de19d2923c5273322ce6f40707d3ef16a29079ce
Artemij <akolesov408@gmail.com>
2022-06-25 17:47:33 +0300

    add time to messageModel

--- Chat/Migrations/20220625143319_time_message.Designer.cs
+++ Chat/Migrations/20220625143319_time_message.Designer.cs
@@ -4,6 +4,7 @@ using Chat.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chat.Migrations
 {
     [DbContext(typeof(ChatDbContex))]
-    partial class ChatDbContexModelSnapshot : ModelSnapshot
+    [Migration("20220625143319_time_message")]
+    partial class time_message
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Chat/Migrations/20220625143319_time_message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chat.Migrations
+{
+    public partial class time_message : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "time",
+                table: "messages",
+                type: "datetime2",
+                nullable: false);
+                
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "time",
+                table: "messages");
+        }
+    }
+}
